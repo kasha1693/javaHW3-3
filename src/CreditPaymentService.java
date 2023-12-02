c04036933a2abb5db23608e8787d44cd7be50f9d
karina <karinashemet@mail.ru>
2023-12-02 19:11:35 +0300

    third commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
 
 
-    public double calculate(double credit, double percent, double month) {
+    public double calculate1(double credit, double percent, double month) {
 
         double payment = credit * percent / 12 * (Math.pow((1 + percent / 12), month)) / (Math.pow((1 + percent / 12), month) - 1);
         return (payment); //Sе = SP /12(1+P/12)N/((1+P/12)N -1) ...
--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,16 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
         double credit = 1_000_000;
-        double percent = 9.99;
+        double percent = 0.0999;
         double month = 12;
 
-        double myCredit = service.calculate(1_000_000, 9.99, 12);
+        double myCredit = service.calculate1(1_000_000, 0.0999, 12);
+        System.out.println(myCredit);
+
+        myCredit = service.calculate1(1_000_000, 0.0999, 24);
+        System.out.println(myCredit);
+
+        myCredit = service.calculate1(1_000_000, 0.0999, 36);
         System.out.println(myCredit);
 
     }
